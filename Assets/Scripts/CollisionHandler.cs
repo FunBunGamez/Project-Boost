d3b177734a109421b096c6e1bff85cc3c0a170a8
Coding Rabbit <codingrabbit@funbungamez.com>
2022-03-31 18:30:29 -0400

    Redesign of the rocket

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -14,6 +14,7 @@ public class CollisionHandler : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        crash =
     }
    
     void OnCollisionEnter(Collision other) 
